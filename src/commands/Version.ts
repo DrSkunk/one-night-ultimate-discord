bcdafa6834033a26c94b76bfc5b7f758303da056
Sebastiaan Jansen <whitebird@live.be>
2021-05-29 02:53:49 +0200

    now correct version added version command

---
+++ src/commands/Version.ts
@@ -0,0 +1,37 @@
+import { Message, MessageEmbed } from 'discord.js';
+import { getDiscordInstance } from '../DiscordClient';
+import { Command } from '../types/Command';
+import packageJson from '../../package.json';
+
+const command: Command = {
+  names: ['version', 'v'],
+  description: 'Get current bot version.',
+  params: [],
+  execute,
+  adminOnly: false,
+};
+
+async function execute(msg: Message): Promise<void> {
+  const client = getDiscordInstance();
+  if (!client) {
+    throw new Error('Discord did not initialize');
+  }
+
+  const embed = new MessageEmbed();
+
+  embed.setTitle('One night ultimate Discord');
+  embed.setDescription(
+    'Visit https://github.com/DrSkunk/one-night-ultimate-discord for source code'
+  );
+  embed.setURL('https://github.com/DrSkunk/one-night-ultimate-discord');
+
+  embed.addField('Version', `v${packageJson.version}`);
+
+  embed.setFooter(
+    'Made with ❤️ by Sebastiaan Jansen / DrSkunk',
+    'https://i.imgur.com/RPKkHMf.png'
+  );
+
+  await msg.channel.send(embed);
+}
+export = command;
